b203f76f4e088fb6d0f71538aa26f7cf36448b10
ColinVenancio <232087417+colinven@users.noreply.github.com>
2026-07-30 19:20:57 -0400

    project scaffold

---
+++ src/main/java/net/shieldshare/shieldshare/ShieldshareApplication.java
@@ -0,0 +1,13 @@
+package net.shieldshare.shieldshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShieldshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShieldshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/shieldshare/shieldshare/ShieldshareApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shieldshare.shieldshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShieldshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
