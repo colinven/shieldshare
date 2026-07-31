1adae017d6e8309a0716d487f6fbdf64be8445fa
ColinVenancio <232087417+colinven@users.noreply.github.com>
2026-07-31 14:08:53 -0400

    enable jps repositories

--- src/main/java/net/shieldshare/shieldshare/ShieldshareApplication.java
+++ src/main/java/net/shieldshare/shieldshare/ShieldshareApplication.java
@@ -2,8 +2,10 @@ package net.shieldshare.shieldshare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ShieldshareApplication {
 
 	public static void main(String[] args) {
