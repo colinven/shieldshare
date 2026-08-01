cdfd06655cb1c98d04c066c4445d51d32f8ab482
ColinVenancio <232087417+colinven@users.noreply.github.com>
2026-08-01 11:28:06 -0400

    refactor GlobalExceptionHandler, add generic fallback for Exception.class to catch any exceptions not listed

--- src/main/java/net/shieldshare/shieldshare/exception/GlobalExceptionHandler.java
+++ src/main/java/net/shieldshare/shieldshare/exception/GlobalExceptionHandler.java
@@ -10,17 +10,27 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(OversizedPayloadException.class)
-    public ResponseEntity<ErrorResponse> handleOversizedPayloadException(OversizedPayloadException e) {
-        HttpStatus status = HttpStatus.CONTENT_TOO_LARGE;
+    // Generate a uniform error response to send to the client
+    private ResponseEntity<ErrorResponse> generateErrorResponse(HttpStatus status, Exception e) {
         ErrorResponse response = new ErrorResponse(LocalDateTime.now(), status.value(), status.getReasonPhrase(), e.getMessage());
         return new ResponseEntity<>(response, status);
     }
 
+    // Generic fallback handler for any exception not listed in this class
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        return generateErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e);
+    }
+
+    @ExceptionHandler(OversizedPayloadException.class)
+    public ResponseEntity<ErrorResponse> handleOversizedPayloadException(OversizedPayloadException e) {
+        return generateErrorResponse(HttpStatus.CONTENT_TOO_LARGE, e);
+    }
+
     @ExceptionHandler(SecretInsertionException.class)
     public ResponseEntity<ErrorResponse> handleSecretInsertionException(SecretInsertionException e) {
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        ErrorResponse response = new ErrorResponse(LocalDateTime.now(), status.value(), status.getReasonPhrase(), e.getMessage());
-        return new ResponseEntity<>(response, status);
+        return generateErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e);
     }
+
+
 }
