67b900ddea3aee3296f134a018ebab435482621b
ColinVenancio <232087417+colinven@users.noreply.github.com>
2026-08-02 15:00:27 -0400

    add exception handlers for invalid Base64 payloads and jakarta field validation errors

--- src/main/java/net/shieldshare/shieldshare/exception/GlobalExceptionHandler.java
+++ src/main/java/net/shieldshare/shieldshare/exception/GlobalExceptionHandler.java
@@ -2,10 +2,14 @@ package net.shieldshare.shieldshare.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,6 +26,15 @@ public class GlobalExceptionHandler {
         return generateErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        Map<String, String> errors = e.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+        ErrorResponse response = new ErrorResponse(LocalDateTime.now(), status.value(), status.getReasonPhrase(), errors.toString());
+        return new ResponseEntity<>(response, status);
+    }
+
     @ExceptionHandler(OversizedPayloadException.class)
     public ResponseEntity<ErrorResponse> handleOversizedPayloadException(OversizedPayloadException e) {
         return generateErrorResponse(HttpStatus.CONTENT_TOO_LARGE, e);
@@ -32,5 +45,9 @@ public class GlobalExceptionHandler {
         return generateErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, e);
     }
 
+    @ExceptionHandler(MalformedPayloadException.class)
+    public ResponseEntity<ErrorResponse> handleMalformedPayloadException(MalformedPayloadException e) {
+        return generateErrorResponse(HttpStatus.BAD_REQUEST, e);
+    }
 
 }
---
+++ src/main/java/net/shieldshare/shieldshare/exception/MalformedPayloadException.java
@@ -0,0 +1,7 @@
+package net.shieldshare.shieldshare.exception;
+
+public class MalformedPayloadException extends RuntimeException {
+    public MalformedPayloadException(String message) {
+        super(message);
+    }
+}
