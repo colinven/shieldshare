08525ab708a95c57f8d6a9cd1d3e8a08fb7e9663
ColinVenancio <232087417+colinven@users.noreply.github.com>
2026-08-12 10:50:27 -0400

    add @RateLimited annotation to map rate limited routes to specific bucket configurations

---
+++ src/main/java/net/shieldshare/shieldshare/ratelimit/RateLimited.java
@@ -0,0 +1,12 @@
+package net.shieldshare.shieldshare.ratelimit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RateLimited {
+    RateLimitedRoute route() default RateLimitedRoute.NONE;
+}
