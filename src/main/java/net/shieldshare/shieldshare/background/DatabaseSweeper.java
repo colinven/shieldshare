9fa3f43b221a7bdaf0da2f9e268ab961eca0000a
ColinVenancio <232087417+colinven@users.noreply.github.com>
2026-08-28 08:14:08 -0400

    configure background sweeper not to run in tests unless explicitly enabled; fixes shutdown race in @SpringBootTest

--- src/main/java/net/shieldshare/shieldshare/background/DatabaseSweeper.java
+++ src/main/java/net/shieldshare/shieldshare/background/DatabaseSweeper.java
@@ -3,11 +3,13 @@ package net.shieldshare.shieldshare.background;
 import lombok.RequiredArgsConstructor;
 import net.shieldshare.shieldshare.repository.AuditLog;
 import net.shieldshare.shieldshare.service.SecretsService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "app.sweeper.enabled", havingValue = "true", matchIfMissing = true)
 public class DatabaseSweeper {
 
     private final SecretsService secretsService;
--- src/test/java/net/shieldshare/shieldshare/ShieldshareApplicationTests.java
+++ src/test/java/net/shieldshare/shieldshare/ShieldshareApplicationTests.java
@@ -3,8 +3,10 @@ package net.shieldshare.shieldshare;
 import net.shieldshare.shieldshare.support.AbstractPostgresIT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ShieldshareApplicationTests extends AbstractPostgresIT {
 
 	@Test
